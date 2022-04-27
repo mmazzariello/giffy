4a260425b1dd8550c78fbee18d7a04a59a39cb4d
Mariana Mazzariello <mfmazzariello@gmail.com>
2022-04-27 16:31:29 +0200

    ADD useEffect, useState & fetch api call to get panda's gifs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import getGifs from "./services/getGifs";
+
+// const GIFS = [
+//   "https://media4.giphy.com/media/jxODdkVOIGFgc/giphy.webp?cid=ecf05e479casxzac6ktwem47j0q5b0jg66jxge3xm41vh262&rid=giphy.webp&ct=g",
+//   "https://media4.giphy.com/media/EatwJZRUIv41G/giphy.gif?cid=ecf05e47r7e963wtgysmdinlcgeexffy3i6y7y3ilbnjqgc8&rid=giphy.gif&ct=g",
+// ];
+
+// const DIFF_GIFS = [
+//   "https://media1.giphy.com/media/H2SRCPod2Bfos/200w.webp?cid=ecf05e479casxzac6ktwem47j0q5b0jg66jxge3xm41vh262&rid=200w.webp&ct=g",
+// ];
 
 function App() {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(function () {
+    console.log("new effect");
+    getGifs({ keyword: "panda" }).then((gifs) => setGifs(gifs));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Panda love</h1>
+      <section className="App-content">
+        {gifs.map((singleGif) => {
+          return (
+            <div>
+              <img src={singleGif.url} alt="" />
+              <p>{singleGif.title}</p>
+              {/* <small>{singleGif.id}</small> */}
+            </div>
+          );
+        })}
+      </section>
     </div>
   );
 }
---
+++ src/services/getGifs.js
@@ -0,0 +1,19 @@
+const apiKey = "IxNOCpTwTtXz4uAS81C6mp8lZ7Tgz1Xi";
+
+export default function getGifs({ keyword = "panda" } = {}) {
+  const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=25&offset=0&rating=g&lang=en`;
+
+  return fetch(apiUrl)
+    .then((res) => res.json())
+    .then((response) => {
+      const { data = [] } = response;
+      if (Array.isArray(data)) {
+        const gifs = data.map((image) => {
+          const { images, title, id } = image;
+          const { url } = images.downsized_medium;
+          return { title, id, url };
+        });
+        return gifs;
+      }
+    });
+}
