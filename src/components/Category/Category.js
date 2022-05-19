0d7297ea0916eff2ef50f056f22a7d3115f75515
Mariana Mazzariello <mfmazzariello@gmail.com>
2022-05-19 13:35:13 +0100

    ADD polyfill to support IntersectionObserver in internet explorer

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -7,15 +7,15 @@ export default function Category({ name, options = [] }) {
   return (
     <div className="Category">
       <h3 className="Category-title">({name})</h3>
-      <ul className="Category-list">
+      <div className="Category-list">
         {options.map((singleOption) => (
-          <li key={singleOption}>
+          <span key={singleOption}>
             <Link className="Category-link" to={`/search/${singleOption}`}>
-              {singleOption}
+              {singleOption} -
             </Link>
-          </li>
+          </span>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
--- src/components/TrendigSearches/trendingSearches.js
+++ src/components/TrendigSearches/trendingSearches.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import getTrendingTerms from "../../services/getTrendinTermsServices";
 import Category from "./../../components/Category/Category";
 
-export default function TrendingSearches() {
+function TrendingSearches() {
   const [trends, setTrends] = useState([]);
 
   useEffect(function () {
@@ -10,4 +10,37 @@ export default function TrendingSearches() {
   }, []);
 
   return <Category name="Trendings" options={trends} />;
+}
+
+export default function LazyTrending() {
+  const [show, setShow] = useState(false);
+  const elementRef = useRef();
+
+  useEffect(function () {
+    let observer;
+
+    const onChange = (entries, observer) => {
+      // console.log("entries", entries);
+      const el = entries[0];
+      // console.log("el", el);
+      if (el.isIntersecting) {
+        setShow(true);
+        observer.disconnect();
+      }
+    };
+
+    Promise.resolve(
+      typeof IntersectionObserver !== "undefined"
+        ? IntersectionObserver
+        : import("intersection-observer").then(() => {
+            observer = new IntersectionObserver(onChange, {
+              rootMargin: "100px",
+            });
+            observer.observe(elementRef.current);
+          })
+    );
+
+    return () => observer && observer.disconnect();
+  });
+  return <div ref={elementRef}>{show ? <TrendingSearches /> : null}</div>;
 }
